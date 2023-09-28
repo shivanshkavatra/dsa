30e0004c7ef4113d17ab860c94bb694f003a2182
Shivansh Kavatra <collegekavatra@gmail.com>
2023-09-28 14:48:30 +0530

    added a file

--- maths/trailingzeroesinfact.cpp
+++ maths/trailingzeroesinfact.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 using namespace std;
 
+int countTrailingZeroes(int n){
+    int res = 0;
+    for (int i = 5; i <= n; i *= 5)
+    {
+        res = res + n/i ;
+    }
+    return res;
+    
+}
+
 int main(){
     int n;
     cout << "Enter a number: ";
     cin >> n;
     
+    cout << "Number of zeroes present in factorial of " << n << " are " << countTrailingZeroes(n) << endl;
     return 0;
 }
